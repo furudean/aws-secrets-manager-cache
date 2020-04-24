b519596bdbc83ce149659dc2af35949bf418fbe7
Cassidy Bandy <31383109+c-bandy@users.noreply.github.com>
2020-04-24 14:21:51 +0200

    chore: kills stray files

--- .release.config.js
+++
@@ -1,8 +0,0 @@
-module.exports = {
-  branches: ['master'],
-  plugins: [
-    '@semantic-release/commit-analyzer',
-    '@semantic-release/release-notes-generator',
-    '@semantic-release/npm',
-  ]
-};
